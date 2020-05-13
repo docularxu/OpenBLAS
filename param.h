f3b897a2517b42b7f4a047b839b603a81985920e
Guodong Xu <guodong.xu@linaro.org>
2020-05-13 13:50:32 +0800

    Add support of new target TSV110SVE
    TSV110SVE is next generation of TSV110 with Arm SVE feature enabled. Vector
    length on TSV110SVE is 256-bit, which allows it to host more double floats
    than previous Arm64 archs.
    
    Kernels of DGEMM and DTRMM are rewritten using SVE assembly language.
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- param.h
+++ param.h
@@ -2647,6 +2647,35 @@ is a big desktop or server with abundant cache rather than a phone or embedded d
 #define CGEMM_DEFAULT_R 4096
 #define ZGEMM_DEFAULT_R 2048
 
+#elif defined(TSV110SVE)
+
+#define SGEMM_DEFAULT_UNROLL_M  16
+#define SGEMM_DEFAULT_UNROLL_N  4
+
+#define DGEMM_DEFAULT_UNROLL_M  16
+#define DGEMM_DEFAULT_UNROLL_N  4
+
+#define CGEMM_DEFAULT_UNROLL_M  8
+#define CGEMM_DEFAULT_UNROLL_N  4
+
+#define ZGEMM_DEFAULT_UNROLL_M  4
+#define ZGEMM_DEFAULT_UNROLL_N  4
+
+#define SGEMM_DEFAULT_P	512
+#define DGEMM_DEFAULT_P	256
+#define CGEMM_DEFAULT_P 256
+#define ZGEMM_DEFAULT_P 128
+
+#define SGEMM_DEFAULT_Q 1024
+#define DGEMM_DEFAULT_Q 512
+#define CGEMM_DEFAULT_Q 512
+#define ZGEMM_DEFAULT_Q 512
+
+#define SGEMM_DEFAULT_R 4096
+#define DGEMM_DEFAULT_R 4096
+#define CGEMM_DEFAULT_R 4096
+#define ZGEMM_DEFAULT_R 2048
+
 #elif defined(THUNDERX)
 
 #define SGEMM_DEFAULT_UNROLL_M  4
